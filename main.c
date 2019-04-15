b172a2fb3408a57723015b33daa0d2f1c27d9a00
Васильев Антон Максимович <ip814s03@mwp12.csc.local>
2019-04-15 13:32:52 +0700

    Change var CV-qualifer

--- main.c
+++ main.c
@@ -30,7 +30,7 @@ int main()
 {
     int arr[] = {3, 1, 4, 1, 5, 9, 2};
 
-    const int min = min_element(arr, N_ELEMENTS(arr));
+    int min = min_element(arr, N_ELEMENTS(arr));
 
     print_array(arr, N_ELEMENTS(arr));
     printf("Min element: %d\n", min);
